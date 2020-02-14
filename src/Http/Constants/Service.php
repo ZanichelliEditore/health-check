ae84db0cecb291f90cb080105528a3b53f028e1c
gsaraceno92 <giuseppe.saraceno@zanichelli.it>
2020-02-14 22:58:51 +0100

    add gitignore, add constants

---
+++ src/Http/Constants/Service.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zanichelli\HealthCheck\Http\Constants;
+
+class Service
+{
+
+    // Possible values for project alert
+    const S3 = 'awsFilesystem';
+    const DATABASE = 'database';
+    const FILESYSTEM = 'filesystem';
+}

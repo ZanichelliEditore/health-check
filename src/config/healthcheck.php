2efadf4726b098d72fec2e99fbf63b7acc840557
gsaraceno92 <giuseppe.saraceno@zanichelli.it>
2020-02-15 00:09:11 +0100

    edit config descriptions

--- src/config/healthcheck.php
+++ src/config/healthcheck.php
@@ -2,14 +2,20 @@
 
 return [
     /**
-     * The parameters can be set in .env as a string with comma separated values
-     * 
      * Delete (or set to null) every configuration that is not necessary
      */
     /**
-     * Use db section to define all database connections to check
+     * //INFO
+     * 1. Use db section to define all database connections to check,
+     *    an array element for every connection you have.
+     * 2. Filesystem section includes s3 check (an element for every possible usage) 
+     *    and local filesystem that comprehends storage size check.
+     *    Define your own min size to evaluate a safe situation for your application.
+     * 
+     * Restore the code below to use default package configuration.
      */
-    'db' => [
+
+    /* 'db' => [
         [
             'connection' => env('DB_CONNECTION', null)
         ]
@@ -20,12 +26,9 @@ return [
                 'disk_name' =>  null
             ]
         ],
-        /**
-         * set value as null if it's not necessary check local filesystem
-         */
         'local' => [
             'volume_path' => '/var/www',
             'free_size_limit' => 1000000000
         ]
-    ]
+    ] */
 ];

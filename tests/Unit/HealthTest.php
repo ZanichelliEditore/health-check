a40799f0a9dc9ad128d62a880ee4ba327cca3e62
gsaraceno92 <giuseppe.saraceno@zanichelli.it>
2020-02-14 22:59:25 +0100

    add unit tests

--- tests/Unit/HealthTest.php
+++ tests/Unit/HealthTest.php
@@ -2,7 +2,8 @@
 
 namespace Zanichelli\HealthCheck\Tests\Unit;
 
-use Mockery;
+use Mockery as m;
+use Zanichelli\HealthCheck\Http\Constants\Service;
 use Zanichelli\HealthCheck\Tests\TestCase;
 use Zanichelli\HealthCheck\Http\Services\HealthCheckService;
 use Zanichelli\HealthCheck\Http\Controllers\HealthController;
@@ -10,241 +11,94 @@ use Zanichelli\HealthCheck\Http\Controllers\HealthController;
 class HealthTest extends TestCase
 {
 
+    /**
+     * @return array
+     */
+    private function getArrayStatus()
+    {
+        return [
+            [
+                'service' => Service::DATABASE . '/mysql',
+                'available' => true,
+                'message' => null,
+                'metadata' => []
+            ],
+            [
+                'service' => Service::S3 . '/s3',
+                'available' => true,
+                'message' => null,
+                'metadata' => []
+            ],
+        ];
+    }
 
     /**
      * @test
      *
      * @return void
      */
-    public function checkServiceTest()
+    public function checkEmptySystem()
     {
-        $mock = Mockery::mock(HealthCheckService::class)->makePartial()
+        $mock = m::mock(HealthCheckService::class)->makePartial()
             ->shouldReceive([
                 'checkSystem' => [],
             ])
             ->withAnyArgs()
             ->once()
-            ->andReturn();
+            ->getMock();
 
-        $this->app->instance('Zanichelli\HealthCheck\Http\Services\HealthCheckService', $mock);
+        $controller = new HealthController($mock);
 
-        $response = $this->get('/api/health');
-        $response->assertStatus(400);
+        $response = $controller->index();
+        $content = $response->getContent();
+
+        $this->assertJson($content, "{}");
+        $this->assertEquals($response->getStatusCode(), 204);
     }
 
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkDbPositiveTest()
-    // {
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'getDbConnection' => true,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkDb(), null);
-    //     $this->assertEquals($healthController->getHealth(), array('db' => array('status' => 'ok')));
-    // }
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function checkSuccesSystems()
+    {
+        $mock = m::mock(HealthCheckService::class)->makePartial()
+            ->shouldReceive([
+                'checkSystem' => $this->getArrayStatus()
+            ])
+            ->withAnyArgs()
+            ->once()
+            ->getMock();
 
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkDbNegativeTest()
-    // {
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'getDbConnection' => false,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkDb(), null);
-    //     $this->assertEquals(
-    //         $healthController->getHealth(),
-    //         array(
-    //             'db' => array(
-    //                 'status' => 'ko',
-    //                 'message' => trans('healthcheck::messages.DatabaseConnectionNotAvailable'),
-    //             )
-    //         )
-    //     );
-    // }
+        $controller = new HealthController($mock);
+        $response = $controller->index();
+        $content = $response->getContent();
 
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkLocalStoragePositiveTest()
-    // {
-    //     $storageMock = Mockery::mock(Storage::disk('local'))->makePartial()
-    //         ->shouldReceive([
-    //             'put' => true,
-    //             'delete' => true,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
 
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'storageDisk' => $storageMock,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->twice()
-    //         ->getMock();
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function checkFailSystems()
+    {
+        $data = $this->getArrayStatus();
+        $data[0]['available'] = false;
+        $mock = m::mock(HealthCheckService::class)->makePartial()
+            ->shouldReceive([
+                'checkSystem' => $data
+            ])
+            ->withAnyArgs()
+            ->once()
+            ->getMock();
 
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkLocalStorage(), null);
-    //     $this->assertEquals($healthController->getHealth(), array('localstorage' => array('status' => 'ok')));
-    // }
+        $controller = new HealthController($mock);
+        $response = $controller->index();
+        $content = $response->getContent();
 
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkLocalStorageNegativeTest()
-    // {
-    //     $storageMock = Mockery::mock(Storage::disk('local'))->makePartial()
-    //         ->shouldReceive([
-    //             'put' => false,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'storageDisk' => $storageMock,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkLocalStorage(), null);
-    //     $this->assertEquals($healthController->getHealth(), array('localstorage' => array(
-    //         'status' => 'ko',
-    //         'message' => trans('healthcheck::messages.UnableToWriteOnDisk', ['storage' => 'local'])
-    //     )));
-    // }
-
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkS3StoragePositiveTest()
-    // {
-    //     $storageMock = Mockery::mock(Storage::disk('s3'))->makePartial()
-    //         ->shouldReceive([
-    //             'files' => array(),
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'storageDisk' => $storageMock,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkS3Storage(), null);
-    //     $this->assertEquals($healthController->getHealth(), array('s3storage' => array('status' => 'ok')));
-    // }
-
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkS3StorageNegativeTest()
-    // {
-    //     $storageMock = Mockery::mock(Storage::disk('s3'))->makePartial()
-    //         ->shouldReceive('files')
-    //         ->andThrow(\Exception::class, 'failed somewhere')
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'storageDisk' => $storageMock,
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkS3Storage(), null);
-    //     $this->assertEquals($healthController->getHealth(), array('s3storage' => array(
-    //         'status' => 'ko',
-    //         'message' => 'failed somewhere'
-    //     )));
-    // }
-
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkFreeSpacePositiveTest()
-    // {
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'disk_free_space' => (config('healthcheck.params.free_size_limit') + 1),
-    //         ])
-    //         ->withAnyArgs()
-    //         ->once()
-    //         ->getMock();
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkFreeSpace(), null);
-    //     $this->assertEquals($healthController->getHealth(), array('freeSpace' => array('status' => 'ok')));
-    // }
-
-    // /**
-    //  * @test
-    //  *
-    //  * @return void
-    //  */
-    // public function checkFreeSpaceNegativeTest()
-    // {
-    //     $mock = Mockery::mock(HealthCheckWrapper::class)->makePartial()
-    //         ->shouldReceive([
-    //             'disk_free_space' => (config('healthcheck.params.free_size_limit') - 1),
-    //         ])
-    //         ->withAnyArgs()
-    //         ->twice()
-    //         ->getMock();
-    //     $this->app->instance('Zanichelli\HealthCheck\Wrappers\HealthCheckWrapper', $mock);
-    //     $healthController = new HealthController($mock);
-    //     $this->assertEquals($healthController->checkFreeSpace(), null);
-    //     $this->assertEquals(
-    //         $healthController->getHealth(),
-    //         array('freeSpace' => array(
-    //             'status' => 'ko',
-    //             'message' => trans('healthcheck::messages.NoDiskSpace', ['space' => (config('healthcheck.params.free_size_limit') - 1)])
-    //         ))
-    //     );
-    // }
+        $this->assertEquals($response->getStatusCode(), 400);
+    }
 }

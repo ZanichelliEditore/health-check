89d49a23d272aeca796436bae12f1ccddec405d9
gsaraceno92 <giuseppe.saraceno@zanichelli.it>
2020-02-15 15:30:09 +0100

    increase code-coverage

---
+++ tests/Feature/HealhTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Zanichelli\HealthCheck\Tests\Feature;
+
+use Zanichelli\HealthCheck\Tests\TestCase;
+
+class HealthTest extends TestCase
+{
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('healthcheck.db', []);
+        $app['config']->set('healthcheck.filesystem.local', []);
+    }
+    
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function checkEmptyConfigurations()
+    {
+        $response = $this->call('GET', 'api/health');
+        $response->assertStatus(204);
+    }
+}

98c5437265e579f1298167f154ba657c0d3fe518
gsaraceno92 <giuseppe.saraceno@zanichelli.it>
2020-02-15 15:05:09 +0100

    add filesystem feature tests

--- tests/Feature/FilesystemHealthTest.php
+++ tests/Feature/FilesystemHealthTest.php
@@ -16,7 +16,11 @@ class FilesystemHealthTest extends TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('healthcheck.filesystem.local', ['volume_path' => './']);
+        $app['config']->set('healthcheck.filesystem.local', [
+                'volume_path' => './',
+                'free_size_limit' => 1
+            ]
+        );
     }
 
     /**
@@ -33,6 +37,64 @@ class FilesystemHealthTest extends TestCase
                     'service' => Service::FILESYSTEM . '/local',
                     'available' => true,
                     'message' => null,
+                    'metadata' => []
+                ]]
+            ]);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function checkFilesystemNoSpace()
+    {
+        $this->app['config']->set('healthcheck.filesystem.local', [
+                'volume_path' => './',
+                'free_size_limit' => 999999999999999
+            ]
+        );
+
+        $response = $this->call('GET', 'api/health');
+        $content = json_decode($response->getContent());
+        $response->assertStatus(200)
+            ->assertExactJson([
+                'status' => [[
+                    'service' => Service::FILESYSTEM . '/local',
+                    'available' => true,
+                    'message' => null,
+                    'metadata' => [
+                        'freespace' => [
+                            'size' => $content->status[0]->metadata->freespace->size,
+                            'unit' => 'Megabyte',
+                            'message' => trans('healthcheck::messages.filesystem.NoDiskSpace')
+                        ]
+                    ]
+                ]]
+            ]);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function checkFilesystemError()
+    {
+        $this->app['config']->set('healthcheck.filesystem.local', [
+                'disk_name' => 'test',
+                'volume_path' => './'
+            ]
+        );
+
+        $response = $this->call('GET', 'api/health');
+        $content = json_decode($response->getContent());
+        $response->assertStatus(400)
+            ->assertExactJson([
+                'status' => [[
+                    'service' => Service::FILESYSTEM . '/test',
+                    'available' => false,
+                    'message' => trans('healthcheck::messages.DiskNotAvailable'),
                     'metadata' => []
                 ]]
             ]);

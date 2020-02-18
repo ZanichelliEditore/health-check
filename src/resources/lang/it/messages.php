08a2accce575f555cef385b23736f9e90c44aeff
Michela Tarozzi <mtarozzi@zanichelli.it>
2020-02-18 14:53:13 +0100

    :fire: remove unused messages

--- src/resources/lang/it/messages.php
+++ src/resources/lang/it/messages.php
@@ -2,9 +2,7 @@
 
 return [
     'DatabaseConnectionNotAvailable' => 'La connessione con il database non è disponibile',
-    'UnableToWriteOnDisk' => 'Impossibile scriveresul disco :storage',
     'DiskNotAvailable' => 'Filesystem non disponibile',
-    'SystemError' => 'Errore di sistema, contatta l\'amministratore',
     'ErrorConnectionS3' => 'Impossibile connettersi a S3',
     'filesystem' => [
         'WritingError' => 'Errore durante la scrittura',
